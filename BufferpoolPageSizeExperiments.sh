a8473517ddd22e0202e888f80ebc9b2cd53da4eb
tommyho973 <59982850+tommyho973@users.noreply.github.com>
2025-05-06 16:08:04 -0400

    Update BufferpoolPageSizeExperiments.sh

--- BufferpoolPageSizeExperiments.sh
+++ BufferpoolPageSizeExperiments.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 
-# Inside the FEMU emulator run the following before proceeding with the experiment to create a testfile within the SSD
-sudo parted /dev/nvme0n1 -- mklabel gpt
-sudo parted /dev/nvme0n1 -- mkpart primary ext4 0% 100%
-sudo mkfs.ext4 /dev/nvme0n1p1
-sudo mkdir -p /mnt/myssd
-sudo mount /dev/nvme0n1p1 /mnt/myssd
-
-# Run this portion from within the ACE directory in order to copy the testfile directly into the mounted SSD
-sudo cp testfile3 /mnt/myssd/testfile
-
-# This next portion is the actual experiment it should be run within each policy folder
+# Should be run within each policy folder
 # Important to note that for consistent results set -n parameter to a specific value such as 22 specific once the disk page size is large enough
 
 # Testing with ACE
